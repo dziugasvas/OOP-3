85c504cff577e8eb530eed5f8d11faa20bc19f60
dziugasvas <dziugasvas@gmail.com>
2026-05-11 14:26:29 +0300

    Add Vector class basic structure with ReAlloc

---
+++ include/Vector.h
@@ -0,0 +1,35 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <stdexcept>
+#include <algorithm>
+
+template <typename T>
+class Vector {
+private:
+    T* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+    void ReAlloc(size_t newCapacity) {
+        T* newBlock = new T[newCapacity];
+        if (newCapacity < size_)
+            size_ = newCapacity;
+        for (size_t i = 0; i < size_; i++)
+            newBlock[i] = std::move(data_[i]);
+        delete[] data_;
+        data_ = newBlock;
+        capacity_ = newCapacity;
+    }
+
+public:
+    Vector() { ReAlloc(2); }
+
+    ~Vector() { delete[] data_; }
+
+    size_t size() const { return size_; }
+    size_t capacity() const { return capacity_; }
+    bool empty() const { return size_ == 0; }
+};
+
+#endif
